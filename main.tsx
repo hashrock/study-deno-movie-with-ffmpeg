5fc989b15880bc9e18cea042c7d197db4b57c7d5
hashrock <hashrock@users.noreply.github.com>
2023-12-05 23:01:43 +0900

    it works

--- main.tsx
+++ main.tsx
@@ -12,39 +12,68 @@ const imageSize = {
   height: 488,
 };
 const imageScale = 0.8;
-const image1 = await imageToBase64("./walk-deno-1.png");
-const res = new ImageResponse(
-  <div
-    style={{
-      width: "100%",
-      height: "100%",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      gap: 64,
-      background: "#f5dd42",
-    }}
-  >
+
+const images = [
+  await imageToBase64("./walk-deno-1.png"),
+  await imageToBase64("./walk-deno-2.png"),
+  await imageToBase64("./walk-deno-3.png"),
+  await imageToBase64("./walk-deno-2.png"),
+];
+
+function render(index: number) {
+  const jumpHeightMax = 96;
+  const y = Math.abs(Math.sin(index * Math.PI * 2 / 16)) * jumpHeightMax;
+  const transform = `translateY(${-16 - y}px) scale(${
+    y / jumpHeightMax / 5 + 1
+  })`;
+
+  const imageIndex = Math.floor(index / 4) % images.length;
+
+  const textTransform = `translateY(${
+    Math.sin(index * Math.PI * 2 / 64) * 10
+  }px)`;
+
+  return new ImageResponse(
     <div
       style={{
-        marginTop: 64,
-        fontSize: 64,
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 64,
+        background: "#f5dd42",
       }}
     >
-      Hello Deno!
-    </div>
-    <img
-      style={{
-        transform: `translateY(-64px)`,
-        width: imageSize.width * imageScale,
-        height: imageSize.height * imageScale,
-      }}
-      src={image1}
-    />
-  </div>,
-);
+      <div
+        style={{
+          transform: textTransform,
+          marginTop: 64,
+          fontSize: 64,
+        }}
+      >
+        Hello Deno!
+      </div>
+      <img
+        style={{
+          transform,
+          width: imageSize.width * imageScale,
+          height: imageSize.height * imageScale,
+        }}
+        src={images[imageIndex]}
+      />
+    </div>,
+  );
+}
 
-if (res.body) {
-  Deno.writeFile("image.png", res.body);
+for (let i = 0; i < 64; i++) {
+  const res = await render(i);
+  Deno.mkdir("images", { recursive: true });
+  if (res.body) {
+    Deno.writeFile(
+      `images/image_${i.toString().padStart(2, "0")}.png`,
+      res.body,
+    );
+  }
 }
